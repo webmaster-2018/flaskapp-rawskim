906704bb9790fc33ab26b5d15c9a9909a40f7d80
rawskim <michalrawski01@gmail.com>
2019-03-10 12:50:38 +0100

    edycja szablonu, indexu, listy

--- uczniowie/views.py
+++ uczniowie/views.py
@@ -1,33 +1,79 @@
-# -*- coding: utf-8 -*-
-# quiz-orm/views.py
+#!/usr/bin/env python
+# -- coding: utf-8 --
 
 from flask import Flask
-from flask import render_template, request, redirect, url_for, abort, flash
+from flask import render_template, request, flash, redirect, url_for, abort
 from modele import *
 from forms import *
 
 app = Flask(__name__)
 
-@app.route('/')
+# widok domyślny
+
+
+@app.route("/")
 def index():
-    """Strona główna"""
     return render_template('index.html')
 
-@app.route("/dodaj_klase", methods=['GET', 'POST'])
-def dodaj_klase():
-    form = KlasaForm()
+@app.route("/lista")
+def lista():
+    uczen = Uczen.select()
+    return render_template('lista.html', uczen=uczen)
 
-    if form.validate_on_submit():
-        Klasa(nazwa=form.nazwa.data, rok_naboru=form.rok_naboru.data,
-              rok_matury=form.rok_matury.data).save()
-        flash("Klasa dodana", 'alert alert-success')
-        return redirect(url_for('lista_klas'))
-    elif request.method == 'POST':
-        flash("Nie uzupełniono wymaganych pól", 'alert alert-danger')
-
-    return render_template('dodaj_klase.html', form=form)
 
 @app.route("/lista_klas")
 def lista_klas():
-    klasy = Klasa.select().order_by(Klasa.rok_naboru, Klasa.nazwa)
-    return render_template('lista_klas.html', klasy=klasy)
+    klasa = Klasa.select()
+    return render_template('lista_klas.html', klasa=klasa)
+
+
+@app.route("/dodaj", methods=['GET', 'POST'])
+def dodaj():
+
+    form = DodajForm()
+    form.klasa.choices = [(k.id, k.klasa)
+                          for k in Klasa.select()]
+    form.plec.choices = [(p.id, p.plec_nazwa)
+                           for p in Plec.select()]
+
+    if form.validate_on_submit():
+        u = Uczen(imie=form.imie.data,
+                    nazwisko=form.nazwisko.data,
+                    plec=form.plec.data,
+                    klasa=form.klasa.data)
+        u.save()
+
+        flash("Dodano ucznia: {} {}".format(
+            form.imie.data, form.nazwisko.data))
+        return redirect(url_for('lista'))
+
+    return render_template('dodaj.html', form=form)
+
+@app.route("/dodaj_klasy", methods=['GET', 'POST'])
+def dodaj_klasy():
+
+    form = DodajKlasaForm()
+
+    if form.validate_on_submit():
+        k = Klasa(klasa=form.klasa.data,
+                  rok_naboru=form.rok_naboru.data,
+                  rok_matury=form.rok_matury.data
+        )
+        k.save()
+
+        flash("Dodano klasę: {}".format(
+            form.klasa.data))
+        return redirect(url_for('lista_klas'))
+
+    return render_template('dodaj_klasy.html', form=form)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+def get_or_404(id):
+    try:
+        s = Uczen.get_by_id(id)
+        return s
+    except Uczen.DoesNotExist:
+		abort(404)
